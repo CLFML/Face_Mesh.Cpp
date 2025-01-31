0a99fe0d7b1e03c19e8aab3d2e528b29aa3566da
Victor Hogeweij <hogeweyv@gmail.com>
2025-01-31 23:44:21 +0100

    -add ros2 support to face mesh

--- bindings/ros2/face_mesh_node.cpp
+++ bindings/ros2/face_mesh_node.cpp
@@ -20,12 +20,12 @@
  *
  */
 #include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/msg/image.hpp"
-#include "sensor_msgs/msg/region_of_interest.hpp"
 #include "cv_bridge/cv_bridge.hpp"
 #include "std_msgs/msg/int32.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
 #include "sensor_msgs/point_cloud2_iterator.hpp"
+#include "sensor_msgs/msg/image.hpp"
+#include "sensor_msgs/msg/region_of_interest.hpp"
 #include "std_srvs/srv/set_bool.hpp"
 #include "face_mesh.hpp"
 
@@ -43,38 +43,27 @@ public:
 private:
     void declare_and_get_parameters()
     {
-        declare_parameter("run_on_demand", false);
-        declare_parameter("trigger_topic", "/face_detected");
+        declare_parameter("roi_topic", "/face_roi");
         declare_parameter("camera_topic", "/image_raw");
         declare_parameter("face_mesh_landmarks_topic", "/face_mesh_landmarks");
 
         camera_topic_ = get_parameter("camera_topic").as_string();
         face_landmarks_topic_ = get_parameter("face_mesh_landmarks_topic").as_string();
-        on_demand_topic_ = get_parameter("trigger_topic").as_string();
-        run_continuous_ = !get_parameter("run_on_demand").as_bool();
+        roi_topic_ = get_parameter("roi_topic").as_string();
     }
 
     void setup_communication()
     {
-        if (run_continuous_)
-        {
-            // Link process image straight to the incoming image callback
-            image_sub_ = create_subscription<sensor_msgs::msg::Image>(
-                camera_topic_, rclcpp::SensorDataQoS(),
-                std::bind(&FaceMeshNode::image_callback, this, std::placeholders::_1));
-        }
-        else
-        {
-            // Subscribe to the on-demand trigger topic
-            on_demand_sub_ = create_subscription<std_msgs::msg::Int32>(
-                on_demand_topic_, 10,
-                std::bind(&FaceMeshNode::on_demand_callback, this, std::placeholders::_1));
+        // Subscribe to the on-demand trigger topic
+        on_demand_sub_ = create_subscription<sensor_msgs::msg::RegionOfInterest>(
+            roi_topic_, 10,
+            std::bind(&FaceMeshNode::on_demand_callback, this, std::placeholders::_1));
 
-            // Subscribe to the image topic but only store the latest image
-            image_sub_ = create_subscription<sensor_msgs::msg::Image>(
-                camera_topic_, rclcpp::SensorDataQoS(),
-                std::bind(&FaceMeshNode::store_latest_image, this, std::placeholders::_1));
-        }
+        // Subscribe to the image topic but only store the latest image
+        image_sub_ = create_subscription<sensor_msgs::msg::Image>(
+            camera_topic_, rclcpp::SensorDataQoS(),
+            std::bind(&FaceMeshNode::store_latest_image, this, std::placeholders::_1));
+
         face_mesh_landmarks_pub_ = create_publisher<sensor_msgs::msg::PointCloud2>(face_landmarks_topic_, 10);
 
         toggle_service_ = create_service<std_srvs::srv::SetBool>(
@@ -97,7 +86,9 @@ private:
             RCLCPP_ERROR(get_logger(), "cv_bridge exception: %s", e.what());
             return;
         }
-        det_.load_image(cv_ptr->image);
+        
+        cv::Mat cropped_image_to_roi = cv_ptr->image(roi_);
+        det_.load_image(cropped_image_to_roi, roi_);
 
         publish_face_mesh_landmarks();
     }
@@ -106,16 +97,20 @@ private:
         last_image_msg_ = msg; // Store the latest image in memory
     }
 
-    void on_demand_callback(const std_msgs::msg::Int32::SharedPtr msg)
+    void on_demand_callback(const sensor_msgs::msg::RegionOfInterest::SharedPtr msg)
     {
-        if (!detection_enabled_ || msg->data == 0)
+        if (!detection_enabled_ || msg->width == 0 || msg->height == 0)
             return;
-
-        RCLCPP_INFO(get_logger(), "Trigger received! Processing latest image...");
 
         if (last_image_msg_)
         {
-            image_callback(last_image_msg_); // Process the last stored image
+            roi_ = cv::Rect(msg->x_offset, msg->y_offset, msg->width, msg->height);
+            if (roi_.x >= 0 && roi_.y >= 0 &&
+                roi_.x + roi_.width <= last_image_msg_->width &&
+                roi_.y + roi_.height <= last_image_msg_->height)
+            {
+                image_callback(last_image_msg_); // Process the last stored image
+            }
         }
         else
         {
@@ -179,7 +174,7 @@ private:
     }
 
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
-    rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr on_demand_sub_;
+    rclcpp::Subscription<sensor_msgs::msg::RegionOfInterest>::SharedPtr on_demand_sub_;
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr face_mesh_landmarks_pub_;
     rclcpp::Service<std_srvs::srv::SetBool>::SharedPtr toggle_service_;
 
@@ -188,9 +183,9 @@ private:
     CLFML::FaceMesh::FaceMesh det_;
     std::string camera_topic_;
     std::string face_landmarks_topic_;
-    std::string on_demand_topic_;
+    std::string roi_topic_;
+    cv::Rect roi_ = cv::Rect(0, 0, 0, 0);
     bool detection_enabled_;
-    bool run_continuous_;
 };
 
 int main(int argc, char **argv)
